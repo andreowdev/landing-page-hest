b983d83a611a591f53fec3414439ab0674fd26cb
andreowdev <andreohenriqueleite@gmail.com>
2024-09-11 04:15:04 -0400

    code refactoring

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -3,29 +3,26 @@ import { faPhoneVolume, faFlag, faGear, faShuffle } from '@fortawesome/free-soli
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Features() {
+
+  const icons = [
+    { text: "Desenvolve soluções personalizadas e oferece suporte técnico especializado.", color:"#1B262C", title:"Suporte e Consultoria Especializados", icon:faPhoneVolume },
+    { text: "Fornece análise de dados e geração de relatórios para melhor tomada de decisão.", color:"#1B262C", title:"Análise e Relatórios", icon:faFlag },
+    { text: "Automatiza processos e melhora a eficiência operacional dos clientes.", color:"#1B262C",  title:"Ênfase na Eficiência Operacional", icon:faGear },
+    { text: "Oferece soluções escaláveis e flexíveis para se adaptar ao crescimento empresarial.", color:"#1B262C", title:"Flexibilidade e Escalabilidade", icon:faShuffle},
+  ];
+
+
   return (
     <div className="text-center" id=''>
       <h1 className="text-black text-4xl font-bold mt-24 mb-4 ">Características</h1>
       <hr className="border-b-4 border-black w-36 mx-auto mb-8" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-black">
-        {/* Primeira linha de ícones */}
-        <FeatureItem icon={faPhoneVolume} color="#1B262C" title="Suporte e Consultoria Especializados">
-          Desenvolve soluções personalizadas e oferece suporte técnico especializado.
-        </FeatureItem>
-        
-        <FeatureItem icon={faFlag} color="#1B262C" title="Análise e Relatórios">
-          Fornece análise de dados e geração de relatórios para melhor tomada de decisão.
-        </FeatureItem>
-
-        {/* Segunda linha de ícones */}
-        <FeatureItem icon={faGear} color="#1B262C" title="Ênfase na Eficiência Operacional">
-          Automatiza processos e melhora a eficiência operacional dos clientes.
-        </FeatureItem>
-
-        <FeatureItem icon={faShuffle} color="#1B262C" title="Flexibilidade e Escalabilidade">
-          Oferece soluções escaláveis e flexíveis para se adaptar ao crescimento empresarial.
-        </FeatureItem>
+        {icons.map((feature, index) => (
+          <FeatureItem key={index} color={feature.color} title={feature.title} icon={feature.icon}>
+            <p>{feature.text}</p>
+          </FeatureItem>
+        ))}
       </div>
     </div>
   );
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,94 +6,82 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Services() {
+
+    const services = [
+        {
+            src: Hest,
+            alt: "Aplicativo HEST",
+            style: "w-full h-54 object-cover rounded-t-lg",
+            title: "HEST Gerenciador",
+            description: "Aumente a produtividade e REDUZA os seus PROBLEMAS",
+            features: [
+                "Aplicativo", 
+                "Sistema Desktop", 
+                "Controle de pedidos", 
+                "Gestão de Funcionários"
+            ],
+            icon: faCheck,
+            href: "https://api.whatsapp.com/send/?phone=%2B559285650879&text&type=phone_number&app_absent=0",
+            buttonLabel: "CONTRATAR AGORA"
+        },
+        {
+            src: OutroServico,
+            alt: "Outro Serviço",
+            style: "w-full h-54 object-cover rounded-t-lg",
+            title: "Em Breve",
+            description: "",
+            features: [],
+            buttonLabel: "Em Breve"
+        },
+        {
+            src: MaisUmServico,
+            alt: "Mais um Serviço",
+            style: "w-full h-54 object-cover rounded-t-lg",
+            title: "Em Breve",
+            description: "",
+            features: [],
+            buttonLabel: "Em Breve"
+        }
+    ];
+
     return (
-        <div className="text-center bg-slate-200 py-12 ">
+        <div className="text-center bg-slate-200 py-12 text-white">
             <div className="max-w-4xl mx-auto">
                 <h1 className="text-black text-4xl font-bold mb-3" id="serviços">Nossos serviços</h1>
                 <hr className="border-b-4 border-black w-44 mx-auto mb-8" />
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                    {/* Card 1 */}
-                    <div className="max-w-md mx-auto bg-[#3282B8] rounded-lg overflow-hidden shadow-lg h-full">
-                        <img 
-                            src={Hest} 
-                            alt="Aplicativo HEST" 
-                            className="w-full h-54 object-cover rounded-t-lg"
-                        />
-                        <div className="p-6 h-full">
-                            <h2 className="text-white text-4xl font-bold mb-2">H e s T Gerenciador</h2>
-                            <p className="text-white text-sm mb-3">
-                                Aumente a produtividade e <strong>REDUZA</strong> os seus <strong>PROBLEMAS</strong>
-                            </p>
-                            <ul className="list-disc flex-1">
-                                <li className="mb-2 flex items-center text-white">
-                                    <FontAwesomeIcon icon={faCheck} className="mr-2 text-white" />
-                                    Aplicativo
-                                </li>
-                                <li className="mb-2 flex items-center text-white">
-                                    <FontAwesomeIcon icon={faCheck} className="mr-2 text-white" />
-                                    Sistema Desktop
-                                </li>
-                                <li className="mb-2 flex items-center text-white">
-                                    <FontAwesomeIcon icon={faCheck} className="mr-2 text-white" />
-                                    Controle de pedidos
-                                    </li>
-                                <li className="mb-2 flex items-center text-white">
-                                    <FontAwesomeIcon icon={faCheck} className="mr-2 text-white" />
-                                    Gestão de Funcionários 
-                                </li>
-                            </ul>
-                            <div className="flex justify-center mt-auto">
-                                <ul>
-                                    <li
-                                    className="bg-[#71C9CE] hover:bg-[#5aabb8]
-                                    text-white font-bold py-2 px-4 rounded-full focus:outline-none 
-                                    focus:shadow-outline mt-16" >
-                                        <a href="https://api.whatsapp.com/send/?phone=%2B559285650879&text&type=phone_number&app_absent=0">CONTRATAR AGORA</a>
-                                    </li>
-                                </ul>
+                    {services.map((map, index) => (
+                        <div key="index" className="max-w-md mx-auto bg-[#3282B8] rounded-lg overflow-hidden shadow-lg h-full"> 
+                            <img src={map.src} alt={map.alt} className={map.style} />    
+                            
+                            <div className="p-6 h-full">
+                                <h2>{map.title}</h2>
+                                <p>{map.description}</p>
+                                {map.features.length > 0 && (
+                                    <ul className="list-disc flex-1">
+                                        {map.features.map((feature, i) => (
+                                            <li key={i} className="mb-2 flex items-center">
+                                            <FontAwesomeIcon icon={map.icon} className="mr-2" />
+                                            {feature}
+                                        </li>
+                                        ))}
+                                    </ul>
+                                )}
+                                <div className="flex justify-center mt-auto"> 
+                                    {map.href ? (
+                                        <a href={map.href} className="bg-[#71C9CE] hover:bg-[#5aabb8]  font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline mt-16">
+                                            {map.buttonLabel}
+                                        </a>
+                                    ) : (
+                                        <button className="bg-[#71C9CE] hover:bg-[#5aabb8] text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                                            {map.buttonLabel}
+                                        </button>
+                                    )}
+
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    
-                    {/* Card 2 */}
-                    <div className="max-w-md mx-auto bg-[#3282B8] rounded-lg overflow-hidden shadow-lg h-full">
-                        <img 
-                            src={OutroServico} 
-                            alt="Outro Serviço" 
-                            className="w-full h-54 object-cover rounded-t-lg"
-                        />
-                        <div className="p-6 h-full">
-                            <h2 className="text-white text-4xl font-bold mb-2">Em Breve</h2>
-                            <p className="text-white text-sm mb-3">
-                                Em Breve
-                            </p>
-                            <div className="flex justify-center mt-auto">
-                                <button className="bg-[#71C9CE] hover:bg-[#5aabb8] text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-                                    Em Breve
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    {/* Card 3 */}
-                    <div className="max-w-md mx-auto bg-[#3282B8] rounded-lg overflow-hidden shadow-lg h-full">
-                        <img 
-                            src={MaisUmServico} 
-                            alt="Mais um Serviço" 
-                            className="w-full h-54 object-cover rounded-t-lg"
-                        />
-                        <div className="p-6 h-full">
-                            <h2 className="text-white text-4xl font-bold mb-2">Em Breve</h2>
-                            <p className="text-white text-sm mb-3">
-                                Em Breve
-                            </p>
-                            <div className="flex justify-center mt-auto">
-                                <button className="bg-[#71C9CE] hover:bg-[#5aabb8] text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-                                    Em Breve
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
